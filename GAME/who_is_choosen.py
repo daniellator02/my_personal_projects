b0b5aa6564ccb350ddfb86d73ec297f4cc98379a
daniellator02 <daniellator02@gmail.com>
2023-04-29 07:58:42 -0700

    choosen

---
+++ GAME/who_is_choosen.py
@@ -0,0 +1,8 @@
+import random
+
+names_strings = input("Give me everybody's names, seperated by a comma: ").split()
+names = names_strings
+size = len(names)
+ran = random.randint(0,size)
+
+print(f"{names[ran]} is going to pay for the meal.")
